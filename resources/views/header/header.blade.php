00ef7cf8524e81277ee2a1279cb935fd5dccc188
SoksanSerey <sereyoksan16@kit.edu.kh>
2018-07-12 14:51:26 +0700

    change view

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -1,84 +1,69 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>vKirirom Pine Resort</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="keywords" content="Cambodia Resort , Pine resort Cambodia, Kirirom resort, Mountain resort cambodia">
-    <meta name="description" content="vKirirom Pine Resort located on top of kirirom mountain national park provides you once in a life time experiences. A lot of activities in the fresh air like Jungle trekking, horse riding, fruit picking, team building for corporation as well with new creative delicious cuisine contains organic vegetable from own farm">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link rel="icon" href="{{asset('frontend/images/HomePage/favicon.png')}}" type="image/x-icon" />
-    <link href="{{asset('frontend/css/lightbox.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{asset('frontend/css/style.css')}}" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    <!-- build:css mfb.css -->
-    <link href="{{asset('frontend/css/mfb.css')}}" rel="stylesheet">
-    <!-- <link href="{{asset('frontend/css/checkin.css')}}" rel="stylesheet"> -->
-    <!-- build:js mfb.js -->
-    <script type="text/javascript" src="{{asset('frontend/js/jquery.min.js')}}"></script>
-    <script type="text/javascript" src="{{asset('frontend/js/jquery.easing.1.3.js')}}"></script>
-    <script type="text/javascript" src="{{asset('frontend/js/camera.min.js')}}"></script>
-    <script type="text/javascript" src="{{asset('frontend/js/jquery.lightbox.js')}}"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="{{asset('frontend/js/mfb.js')}}"></script>
+<html class="no-js">
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>vKirirom Pine Resort</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+	<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+	<!-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'> -->
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="{{asset('frontend/css/animate.css')}}">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="{{asset('frontend/css/icomoon.css')}}">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="{{asset('frontend/css/bootstrap.css')}}">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
 
-    <!--Start drop menu-->
-    <script>
-        $(function () {
-            var pull = $('#pull');
-            menu = $('nav ul');
-            menuHeight = menu.height();
+	<!-- Modernizr JS -->
+	<script src="{{asset('frontend/js/modernizr-2.6.2.min.js')}}"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
 
-            $(pull).on('click', function (e) {
-                e.preventDefault();
-                menu.slideToggle();
-            });
-
-            $(window).resize(function () {
-                var w = $(window).width();
-                if (w > 320 && menu.is(':hidden')) {
-                    menu.removeAttr('style');
-                }
-            });
-        });
-    </script>
-    <!--End drop menu-->
-    <script src="{{asset('frontend/js/fbplugin.js')}}"></script>
-
-</head>
-
-<body>
-    <!--start-header-->
-    <div class="header">
-        <div class="wrap">      
-            <div class="top-header">
-                <div class="logo">
-                    <a href="welcome"><img src="{{asset('frontend/images/HomePage/vKirirom.svg')}}" alt="vKirirom logo" title="logo" /></a>
-                </div>
-                <div class="social-icons">
-                    <ul>
-                        <li>
-                            <a href="https://www.facebook.com/vkirirom" target="_blank"><img src="{{asset('frontend/images/facebook.svg')}}" alt="www.facebook.com" title="facebook" /></a>
-                        </li>
-                        <li>
-                            <a href="https://www.youtube.com/channel/UCCIu3JTn0s4UX5Kze5Hse0g" target="_blank"><img src="{{asset('frontend/images/youtube.svg')}}" alt="www.youtube.com" title="youtube" /></a>
-                        </li>
-                        <li>
-                            <a href="https://www.instagram.com/vkirirom/" target="_blank"><img src="{{asset('frontend/images/instagram.svg')}}" alt=" www.instagram.com" title="instagram" /></a>
-                        </li>
-                        <li id="book-button"><a href="reservation.php">BOOK NOW</a></li>
-                    </ul>
-                    <div class="facebookprior facebook ">
-                        <div class="fb-like" data-href="https://www.facebook.com/vkirirom" data-width="250" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
-                    </div>
-                </div>
-                <div class="clear"> </div>
+	</head>
+	<body>
+	<div id="fh5co-page">
+	<header id="fh5co-header" role="banner">
+		<div class="container">
+			<div class="row">
+				<div class="header-inner">
+					<a href="{{ route('home') }}"><img src="{{asset('frontend/images/HomePage/vKirirom.svg')}}" alt="vKirirom logo" title="logo"/></a>
+					<nav role="navigation">
+						<ul>
+							<li class="cta"><a href="contact.html">Contact us</a></li>
+							<li class="zoom"><a href="https://www.facebook.com/vkirirom" target="_blank"><img src="{{asset('frontend/images/facebook.svg')}}" alt="www.facebook.com" title="facebook" /></a></li>
+							<li class="zoom"><a href="https://www.youtube.com/channel/UCCIu3JTn0s4UX5Kze5Hse0g" target="_blank"><img src="{{asset('frontend/images/youtube.svg')}}" alt="www.youtube.com" title="youtube" /></a></li>
+							<li class="zoom"><a href="https://www.instagram.com/vkirirom_pine_resort/" target="_blank"><img src="{{asset('frontend/images/instagram.svg')}}" alt="www.instagram.com" title="instagram"/></a></li>
+						</ul>
+					</nav>
+				</div>
             </div>
+          
         </div>
-    </div>
-    <!-- -End-header- -->
-    <div class="clear"></div>
-</body>
+       
+    </header>
+    
+    </div>	
+    	
+    
+	<!-- jQuery -->
+	<script src="{{asset('frontend/js/jquery.min.js')}}"></script>
+	<!-- jQuery Easing -->
+	<script src="{{asset('frontend/js/jquery.easing.1.3.js')}}"></script>
+	<!-- Bootstrap -->
+	<script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+	<!-- Waypoints -->
+	<script src="{{asset('frontend/js/jquery.waypoints.min.js')}}"></script>
+	<!-- Flexslider -->
+	<script src="{{asset('frontend/js/jquery.flexslider-min.js')}}"></script>
 
+	<!-- MAIN JS -->
+	<script src="{{asset('frontend/js/main.js')}}"></script>
+	</body>
 </html>
