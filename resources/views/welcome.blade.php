e56486ca6df8ec708e125545699587c31395231b
SoksanSereyMac <sereysoksan16@kit.edu.kh>
2018-09-17 14:27:44 +0700

    button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,28 +7,30 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>vKirirom Pine Resort</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+	
 	<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
-
+	
 	<link rel="stylesheet" href="{{asset('frontend/css/animate.css')}}">
 	<link rel="stylesheet" href="{{asset('frontend/css/icomoon.css')}}">
 	<link rel="stylesheet" href="{{asset('frontend/css/bootstrap.css')}}">
 	<link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}">
-
 	<!-- Flexslider  -->
 	<link rel="stylesheet" href="{{asset('frontend/css/flexslider.css')}}">
 	<!-- Theme style  -->
 	<link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
 	<link rel="stylesheet" href="{{asset('frontend/css/jquery-ui.css')}}">
-	
+	<!-- <link rel="stylesheet" href="{{asset('css/multirange.css')}}"> -->
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css"> -->
 	<link rel="stylesheet" href="{{asset('frontend/css/bootstrap-select.min.css')}}">
 	<script src="{{asset('frontend/js/modernizr-2.6.2.min.js')}}"></script>
+
+	<!-- <link href="{{asset('frontend/plugin/plugins/bootstrap-select/css/bootstrap-select.css')}}" rel="stylesheet" /> -->
 
 </head>
 
 <body>
 	<div id="fh5co-page">
-		@include('_partial.header')
+	@include('_partial.header')
 		<div id="best-deal">
 			<div class="container-fluid">
 				<div class="row">
@@ -36,49 +38,45 @@
 						<div class="sidebar-filter">
 							<div class="nice well-small">
 								<ul class="nav nav-list">
-									<h4 class="filters-header-text"><img src="{{asset('img/filter.svg')}}" width="20px" height="20px" alt="filter">Filter
-										by :</h4>
+									<h4 class="filters-header-text"><img src="{{asset('img/filter.svg')}}" width="20px" height="20px" alt="filter">Filter by :</h4>
 									<form action="">
-										<div class="filter-box">
+										<div class="col-md-12 col-lg-12 filter-box">
 											<div class="filter-class">
 												<span>Accommodations Class</span><br>
 											</div>
-											<input type="radio" name="accclass" value="budget" id="budget"><span id="budget"> budgets</span><br>
-											<input type="radio" name="accclass" value="luxury" id="luxury"><span id="luxury"> Luxury</span><br>
-											<input type="radio" name="accclass" value="other" id="other"><span id="others"> Other</span>
-											<hr>
+											<input type="radio" name="acc-class" value="budget" id="budget"><span id="budget"> budgets</span><br>
+											<input type="radio" name="acc-class" value="luxury" id="luxury"><span id="budget"> Luxury</span><br>
+											<input type="radio" name="acc-class" value="other" id="other"><span id="budget"> Other</span><hr>
 										</div>
 									</form>
 									<form action="">
-										<div class="filter-box">
+										<div class="col-md-12 col-lg-12 filter-box">
 											<div class="filter-class">
 												<span>Type of beds</span><br>
 											</div>
-											<input type="radio" name="type" value="single"><span id="budget"> Single bed</span><br>
-											<input type="radio" name="type" value="beds"><span id="budget"> 4 beds</span><br>
-											<input type="radio" name="type" value="double"><span id="budget"> Double beds</span><br>
-											<input type="radio" name="type" value="camp"><span id="budget"> Camping type</span>
-											<hr>
+											<input type="radio" name="type-bed" value="single"><span id="budget"> Single bed</span><br>
+											<input type="radio" name="type-bed" value="double"><span id="budget"> Double beds</span><br>
+											<input type="radio" name="type-bed" value="beds"><span id="budget"> 4 beds</span><br>
+											<input type="radio" name="type-bed" value="camp"><span id="budget"> Camping type</span><hr>
 										</div>
 									</form>
-									<li> <a class="totalInCart"><strong>Total Amount <span class="badge badge-warning pull-right" style="line-height:18px;">$448.42</span></strong></a></li>
 								</ul>
 							</div>
 						</div>
 					</div>
-					<div class="col-md-8 col-sm-12 col-xs-12 col-lg-3 float-right">
+					<div class="col-md-9">
 						@foreach($user as $key=>$item)
-						<div class="col-md-12 item-block animate-box" data-animate-effect="fadeIn">
+						<div class="col-md-12 item-block animate-box" style="float: right;" data-animate-effect="fadeIn">
 							<div class="card">
 								<div class="row">
 									<div class="fh5co-property">
 										<div class="col-md-4 col-sm-12">
 											<div id="roomSlider-{{ $item->id }}" class="carousel slide" data-ride="carousel">
-												<ol class="carousel-indicators">
+												<ul class="carousel-indicators">
 													<li data-target="#roomSlider" data-slide-to="0" class="active"></li>
 													<li data-target="#roomSlider" data-slide-to="1"></li>
 													<li data-target="#roomSlider" data-slide-to="2"></li>
-												</ol>
+												</ul>
 												<div class="carousel-inner">
 													<div class="carousel-item active">
 														<img class="d-block w-100" src="{{asset('/frontend/images/slide_1.jpg')}}" alt="First slide">
@@ -99,7 +97,6 @@
 													<span class="sr-only">Next</span>
 												</a>
 											</div>
-											
 										</div>
 										<div class="col-md-5 col-sm-12">
 											<div class="fh5co-property-innter">
@@ -139,20 +136,15 @@
 						</div>
 						@endforeach
 					</div>
-					<div class="col-md-1 animate-box" data-animate-effect="fadeIn">
-						<div class="button-demo">
-						<a href="/payment">
-							<button id="myBtn" hidden="true" type="button" class="btn btn-warning waves-effect">Book now</button>
-						</a>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>
+		<div class="button-demo">
+			<button id="myBtn" hidden="true" type="button" class="btn btn-warning waves-effect">Book</button>
+		</div>
 		@include('_partial.footer')
 	</div>
-	<input type="button" id="testbtn" value="Click me">
-
+	
 	<script src="{{asset('frontend/js/jquery.min.js')}}"></script>
 	<script src="{{asset('frontend/js/jquery.easing.1.3.js')}}"></script>
 	<!-- <script src="{{asset('frontend/js/bootstrap.js')}}"></script> -->
@@ -166,7 +158,7 @@
 	<script>
 		function myFunction() {
 			// var type=;
-
+			
 			var e = document.getElementById("type");
 			var strUser = e.options[e.selectedIndex].value;
 			console.log(e.value)
@@ -289,7 +281,7 @@
 	</script>
 	<script type="text/javascript" src="{{asset('js/moment.min.js')}}"></script>
 	<script type="text/javascript" src="{{asset('js/bootstrap-datetimepicker.min.js')}}"></script>
-
+	
 	<!-- Check In Check out implement -->
 	<script>
 		$(document).ready(function () {
@@ -346,66 +338,26 @@
 		});
 	</script>
 	<script>
-		$(':radio').mousedown(function (e) {
+		$(':radio').mousedown(function(e){
 			var $self = $(this);
-<<<<<<< HEAD
-			if ($self.is(':checked')) {
-				var uncheck = function () {
-					setTimeout(function () {
-						$self.removeAttr('checked');
-					}, 0);
-=======
 			if( $self.is(':checked') ){
 				var uncheck = function(){
 					setTimeout(function(){
 						$self.removeAttr('checked');
 						},
 					0);
->>>>>>> b07e7271549315681ad9cf92c43a269b6055c391
 				};
-				var unbind = function () {
-					$self.unbind('mouseup', up);
+				var unbind = function(){
+				$self.unbind('mouseup',up);
 				};
-<<<<<<< HEAD
-				var up = function () {
-=======
 				var up = function(){
->>>>>>> b07e7271549315681ad9cf92c43a269b6055c391
 					uncheck();
 					unbind();
 				};
-				$self.bind('mouseup', up);
+				$self.bind('mouseup',up);
 				$self.one('mouseout', unbind);
 			}
 		});
 	</script>
-
-
-	<script>
-		var buttonclicked;
-		$("#testbtn").click(function () {
-			if (buttonclicked != true) {
-				buttonclicked = true;
-				alert("Button is clicked for first time");
-			} else {
-				alert("Button was clicked before");
-			}
-		});
-	</script>
-
-	<script>
-		$(document).ready(function () {
-			$('#budget').on('ifClicked', function (event) {
-				$.ajax({
-					type: 'POST',
-					url: "{{ URL::route('budget') }}",
-					success: function (data) {
-						alert("Success");
-					},
-				});
-			});
-		});
-	</script>
 </body>
-
 </html>
